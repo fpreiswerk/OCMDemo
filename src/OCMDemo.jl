25a557fd6a0a7555c8a1641811fc2525f0875bcd
Frank Preiswerk <frank@bwh.harvard.edu>
2016-10-10 22:10:24 -0400

    Initial commit

--- src/OCMDemo.jl
+++ src/OCMDemo.jl
@@ -1,5 +1,30 @@
 module OCMDemo
 
-# package code goes here
+  using ProgressMeter
+  using DSP
+  using Hwloc
+  using LightXML
+
+  include("Util/Buffer.jl")
+  include("Algorithm/build_lookup_usdata.jl")
+  #include("Algorithm/compute_high_variation_inds.jl")
+  include("Algorithm/compute_Sigma.jl")
+  include("Algorithm/cough_detect.jl")
+  include("Algorithm/kernel_regression.jl")
+  include("Algorithm/Reconstruction.jl")
+  include("Algorithm/render.jl")
+  include("IO/Resource.jl")
+  include("IO/Dataset.jl")
+  include("IO/xml_functions.jl")
+  include("Util/helpers.jl")
+  include("Util/workers.jl")
+
+  export
+    load_datasets_xml,
+    load_experiment_xml,
+    recon_highspeed_inside,
+    render_mmodes,
+    init_workers,
+    stop_workers
 
 end # module
